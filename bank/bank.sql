3cba561a9f3c8943e68081867c87b3f87b04ff73
yis4755 <45478214+yis4755@users.noreply.github.com>
2018-12-12 13:32:47 +0900

    Add files via upload

---
+++ bank/bank.sql
@@ -0,0 +1,8 @@
+"insert into member values (?, ?, ?, ?)"
+
+"update member set tel = ? where id = ?";
+
+"delete from member where id = ?";
+
+"select * from member where id = ?";
+"select * from member";
